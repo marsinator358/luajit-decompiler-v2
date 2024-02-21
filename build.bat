56a3ec2d029ac30bdffc998772ce223102981d11
barongello <wagner@barongello.com.br>
2024-02-20 22:24:58 -0300

    Adding a build.bat file to use with MSVC in command line

---
+++ build.bat
@@ -0,0 +1,13 @@
+cl ^
+  /nologo ^
+  /std:c++20 ^
+  /EHsc ^
+  /O2 ^
+  /Fe:luajit-decompiler-v2.exe ^
+  main.cpp ^
+  ast\*.cpp ^
+  bytecode\*.cpp ^
+  lua\*.cpp ^
+  Comdlg32.lib ^
+  Shlwapi.lib ^
+  User32.lib
